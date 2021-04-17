597b8dc5b86f7f832b91fc4c60210edb8bb7bb94
Constantine Nesterov <connestprog@gmail.com>
2021-04-17 21:27:08 +0300

    Add README.md and LICENSE

--- main.cpp
+++ main.cpp
@@ -9,76 +9,59 @@
 #include "circular_buffer_lockfree_srsw.h"
 #include "circular_buffer_blocked_mrmw.h"
 #include "circular_buffer_lockfree_mrmw.h"
-
 #define READER_COUNT 4
 #define WRITER_COUNT 4
-
 #define ELEMENTS_COUNT 1000u
 
-using namespace std;
-
+// Собственно кольцевой буфер
+//                   хранимый тип данных
+//                          vvvvvvv
 connest::CircularBuffer_mrmw<size_t> queue{16};
+//                                        ^^^^
+//                               размер буфера в элементах
 
+// сумма значений считанных элементов
 std::atomic<size_t> read_counter{0};
+// сумма значений записанных элементов
 std::atomic<size_t> write_counter{0};
+
+// количество считанных элементов
 std::atomic<size_t> amount_readed{0};
+// количество записанных элементов
 std::atomic<size_t> amount_written{0};
 
 
 void reader()
 {
     size_t value{0};
-    size_t value_old{0};
-
     while(true) {
-        bool success = queue.try_pop(value);
-//        bool success = true;
-//        queue.pop_wait(value);
-        if(success) {
+        if(queue.try_pop(value)) {
             ++amount_readed;
-
-
-            assert(value_old < value);
-
             read_counter += value;
-
-//            if(read_counter == write_counter) {
-//                stringstream s;
-//                s << "reader at: "
-//                  << read_counter;
-
-//                std::cout << s.str() << std::endl;
-//            }
         }
-
     }
 
 }
 
 void writter()
 {
-    for(size_t i = 1; i < ELEMENTS_COUNT;)
-    {
-        if(queue.try_push_back(i))
-//        queue.push_back_wait(i);
-        {
+    for(size_t i = 1; i < ELEMENTS_COUNT;) {
+        if(queue.try_push_back(i)) {
             write_counter += i;
             ++i;
-            amount_written++;
+            ++amount_written;
         }
     }
 
-    stringstream s;
+    std::stringstream s;
     s << "writter end with: " << write_counter;
-
     std::cout << s.str() << std::endl;
 }
+
 int main()
 {
-
     std::vector<std::thread> writters;
     std::vector<std::thread> readers;
-
 
     writters.reserve(WRITER_COUNT);
     for(int i = 0; i < WRITER_COUNT; ++i)
@@ -88,9 +71,7 @@ int main()
     for(int i = 0; i < READER_COUNT; ++i)
         readers.emplace_back(reader);
 
-
-
-
+    // не ждем читателей
     for(auto& r : readers)
         r.detach();
 
@@ -101,13 +82,8 @@ int main()
     // чтобы читатели успели дочитать (но, и прочитать слишком много, если в алгоритме ошибка)
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
-    std::cout << "readers end with: " << read_counter << std::endl;
-    std::cout << "difference (expected 0): " << (int)(read_counter - write_counter) << std::endl;
-    std::cout << "readed: " << amount_readed << std::endl;
-    std::cout << "writed: " << amount_written << std::endl;
-
-    assert((read_counter - write_counter) == 0);
+    assert(read_counter  == write_counter);
+    assert(amount_readed == amount_written);
 
     return 0;
-
 }
